9a03f8a0e92c6a9847a14e9c5ea30158aeae2cda
Seth Mwebi <sethmwebi27@gmail.com>
2024-06-22 16:00:07 +0300

    implemented destructive and nondestructive ways of modifying an object

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+const employee = {
+  name: "Seth",
+  streetAddress: "123 Mombasa Rd",
+};
+
+const updateEmployeeWithKeyAndValue = (employee, key, value) => {
+  return {
+    ...employee,
+    [key]: value,
+  };
+};
+
+const destructivelyUpdateEmployeeWithKeyAndValue = (employee, key, value) => {
+  employee[key] = value;
+  return employee;
+};
+
+const deleteFromEmployeeByKey = (employee, key) => {
+  const updatedEmployeeDetail = { ...employee };
+  delete updatedEmployeeDetail[key];
+  return updatedEmployeeDetail;
+};
+
+const destructivelyDeleteFromEmployeeByKey = (employee, key) => {
+  delete employee[key];
+  return employee;
+};
